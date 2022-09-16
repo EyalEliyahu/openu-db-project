7b48b43ca468a689aa792f181e373b9034a0e8e0
eyaliyahu <eyal@jit.io>
2022-09-16 16:51:28 +0300

    init SQL

--- back/src/main.py
+++ back/src/main.py
@@ -2,6 +2,17 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+try:
+    import sqlalchemy as db
+    from sqlalchemy import create_engine
+    from sqlalchemy import *
+    import pymysql
+
+    print("all imported")
+
+except:
+    print("error in importing")
+
 
 @app.get("/")
 async def root():
